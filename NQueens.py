3af02c472a751d758e35493dcc2e7922710d7fe0
Asif Rasheed <b00073877@aus.edu>
2020-04-20 19:33:24 +0400

    N Queens

--- NQueens.py
+++ NQueens.py
@@ -2,20 +2,20 @@
 from constraint import Problem
 from constraint import AllDifferentConstraint
 from time import perf_counter
-#from constraint import MinConflictSolver
-
-
+from constraint import MinConflictsSolver
 
 # Solving using incremental approach
-solver = [] # Stores time taken to solve each N
+times = [] # Stores time taken to solve each N
 N = 1 # Variable
 start,end=0,0
-N = 1
+N = 1 # Number of Queens
 while end-start<20: # The program would wait upto 5 minutes for solving a problem
     problem = Problem()
-    cols = range(N)
-    rows = range(N)
+    
+    cols = range(N) # Number of Columns
+    rows = range(N) # Number of Rows
     problem.addVariables(cols, rows)
+    
     for col1 in cols:
         for col2 in cols:
             if col1 < col2:
@@ -29,15 +29,44 @@ while end-start<20: # The program would wait upto 5 minutes for solving a proble
     start = perf_counter() # Starting Counter
     solutions = problem.getSolutions()
     end = perf_counter() # Ending Counter
-    solver.append((N,end-start))
+    times.append((N,end-start)) # Stores Number of Queens and Time Taken to Solve the Puzzle
 
-    print('Possible Solution for',N,'Queens Puzzle:', end=' ')
-    if len(solutions)>0:
-        solution = [(k, v) for k, v in solutions[-1].items()]
-        for p in solution:
-            print(p,end=' ')
-        print()
-    else:
-        print('No solution')
-    N+=1
+print('Total Time Taken to Solve The Puzzle using Incremental Approach:')
+print('N\t Time')
+for time in times:
+    print(time[0],'\t',str(time[1])+' seconds')
+    
 # Solcing using iterative approach
+print('\nIterative Approach')
+times = [] # Stores time taken to solve each N
+N = 1 # Variable
+start,end=0,0
+N = 1 # Number of Queens
+while end-start<20: # The program would wait upto 5 minutes for solving a problem
+    problem = Problem()
+    
+    cols = range(N) # Number of Columns
+    rows = range(N) # Number of Rows
+    problem.addVariables(cols, rows)
+    
+    for col1 in cols:
+        for col2 in cols:
+            if col1 < col2:
+                problem.addConstraint(
+                    lambda row1, row2, col1=col1,
+                    col2=col2: abs(row1 - row2) !=
+                    abs(col1 - col2) and row1 != row2,
+                    (col1, col2),
+                )
+    problem.setSolver(MinConflictsSolver())
+    start = perf_counter() # Starting Counter
+    solution = problem.getSolution() # Minimum Conflict Solver only returns one solution
+    end = perf_counter() # Ending Counter
+    
+    times.append((N,end-start)) # Stores Number of Queens and Time Taken to Solve the Puzzle
+    N+=1
+
+print('Total Time Taken to Solve The Puzzles using Iterative Approach:')
+print('N\t Time')
+for time in times:
+    print(time[0],'\t',str(time[1])+' seconds')
